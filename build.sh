e225cb01ebe31a570c22c730b10b1519dd2a7ded
Garrison Peacock <garrison.peacock@gmail.com>
2019-10-20 02:25:12 -0700

    Added newlib. Still need to find gcc libs

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ OBJDUMP=arm-none-eabi-objdump
 OBJCOPY=arm-none-eabi-objcopy
 
 ASM_FLAGS="--warn --fatal-warnings -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard"
-CC_FLAGS="-Wall -Wextra -Werror -O2 -nostdlib -nostartfiles -ffreestanding -c -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard"
-LD_FLAGS="-L/opt/cross-compiler/lib/gcc/arm-none-eabi/9.2.0/ -lgcc"
+CC_FLAGS="-Wall -Wextra -Werror -O2 -nostdlib -nostartfiles -ffreestanding -c -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard -I../newlib/include"
+LD_FLAGS="-L../newlib/arm-none-eabi/newlib -lc -lg -lm -lgcc"
 
 mkdir -p build/
 
